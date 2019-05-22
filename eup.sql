f8d73ffd199c943780092987d1dc3fe57c71d01a
TypechoShop <typechoshop@qq.com>
2019-05-22 19:48:06 +0800

    Diamond

--- eup.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE `{db_prefix}block` ADD COLUMN `attack_num` int(10) NOT NULL default '0' AFTER `serverip`;
-
-DELETE FROM `{db_prefix}options` WHERE option_name= 'webscan_seconds';
-DELETE FROM `{db_prefix}options` WHERE option_name= 'webscan_refresh';
-DELETE FROM `{db_prefix}options` WHERE option_name= 'cc_switch';
-
-
-INSERT INTO `{db_prefix}options` (option_name, option_value) VALUES ('attacks','10');
